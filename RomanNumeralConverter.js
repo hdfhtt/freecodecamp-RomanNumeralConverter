d81d8b5aa601c82af5a8507306d143a5bb7f983c
Hadif Hatta <45189724+hdfhtt@users.noreply.github.com>
2022-08-15 12:58:51 +0800

    Create RomanNumeralConverter.js

---
+++ RomanNumeralConverter.js
@@ -0,0 +1,23 @@
+function convertToRoman(num) {
+  const digits = [ 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+  const numerals = [ 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M']
+
+  let i = 12
+
+  let output = String()
+
+  while (num > 0) {
+    let div = Math.floor(num/digits[i])
+    num = num%digits[i]
+
+    while (div--) {
+      output = output.concat(numerals[i])
+    }
+    i--
+  }
+
+  console.log(output)
+  return output
+}
+
+convertToRoman(80)
